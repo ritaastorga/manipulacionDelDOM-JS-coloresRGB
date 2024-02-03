05a157d1a242072b8d43ec8cac1c1b00cda1fa8c
rita <ritaastorga10@gmail.com>
2024-02-03 13:49:57 -0300

    finalicé la lógica del proyecto coloresRGB

--- app.js
+++ app.js
@@ -16,4 +16,24 @@ textoAzul.innerText = azul;
 
 function actualizarColor(rojo, verde, azul) {
   const colorRGB = `rgb(${rojo}, ${verde}, ${azul})`;
+  document.body.style.backgroundColor = colorRGB;
 }
+
+//Para actualizar Red (Rojo)
+inputRojo.addEventListener("change", (e) => {
+  let rojo = e.target.value;
+  textoRojo.innerText = rojo;
+  actualizarColor(rojo, verde, azul);
+});
+//Para actualizar Green (Verde)
+inputVerde.addEventListener("change", (e) => {
+  let verde = e.target.value;
+  textoVerde.innerText = verde;
+  actualizarColor(rojo, verde, azul);
+});
+//Para actualizar Blue (Blue)
+inputAzul.addEventListener("change", (e) => {
+  let azul = e.target.value;
+  textoAzul.innerText = azul;
+  actualizarColor(rojo, verde, azul);
+});
