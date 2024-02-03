0b915332f53075e0116240c9732bd58fea5164f0
rita <ritaastorga10@gmail.com>
2024-02-03 13:35:46 -0300

    cree la primera parte de la lógica del proyecto coloresRGB

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const inputRojo = document.getElementById("rojo");
 const inputVerde = document.getElementById("verde");
 const inputAzul = document.getElementById("azul");
+
+const textoRojo = document.getElementById("textoRojo");
+const textoVerde = document.getElementById("textoVerde");
+const textoAzul = document.getElementById("textoAzul");
+
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+
+textoRojo.innerText = rojo;
+textoVerde.innerText = verde;
+textoAzul.innerText = azul;
+
+function actualizarColor(rojo, verde, azul) {
+  const colorRGB = `rgb(${rojo}, ${verde}, ${azul})`;
+}
